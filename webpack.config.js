4060d8e5f561a01ba89065abdd4319bdc84f96ce
Eric Masiello <eric.j.masiello@gmail.com>
2020-03-12 10:21:34 -0400

    Get CSS modules working and in a separate css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,50 @@
-const { resolve } = require("path");
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const cssExtract = new MiniCssExtractPlugin();
 
 const config = {
-    devtool: "source-map",
-    entry: "./src/index.ts",
+    devtool: 'source-map',
+    entry: './src/index.ts',
     output: {
-        filename: "index.js",
+        filename: 'index.js',
         path: resolve(__dirname, "./build"),
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: ["babel-loader"],
+                use: ['babel-loader'],
                 exclude: /node_modules/,
                 resolve: {
-                    extensions: [".js", ".jsx", ".ts", ".tsx"]
+                    extensions: ['.js', '.jsx', '.ts', '.tsx']
                 }
             },
             {
                 test: /\.(scss|css)$/,
                 exclude: /node_modules/,
                 use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                                context: resolve(__dirname, 'src/components/'),
+                            },
+                        },
+                    },
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
                 ]
             }
         ]
     },
+    plugins: [
+        cssExtract,
+    ]
 };
 
 module.exports = config;
