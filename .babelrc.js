128f3dfab6bb1b38c738e0278269153a1f326556
Eric Masiello <eric.j.masiello@gmail.com>
2020-03-12 17:19:50 -0400

    Add support for building css and exporting as es modules

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,33 @@
 module.exports = {
     presets: [
-        "@babel/preset-env",
+        ['@babel/preset-env', {
+            loose: true,
+            shippedProposals: true,
+            modules: false,
+            targets: {
+              ie: 9,
+            },
+        }],
         "@babel/preset-react",
         "@babel/preset-typescript"
     ],
+    plugins: [
+        [
+            "css-modules-transform", {
+                "preprocessCss": "./css-module-transform.js",
+                "extensions": [".css", ".scss"],
+                // "extractCss": {
+                //     "dir": "./build/stylesheets/",
+                //     "relativeRoot": "./src/",
+                //     "filename": "[path]/[name].css"
+                // },
+                "extractCss": "./build/main.css"
+            }
+        ],
+        // A plugin that enables the re-use of Babel's injected helper code to save on codesize.
+        ['@babel/plugin-transform-runtime', {
+            useESModules: true,
+        }],
+    ]
   };
   
